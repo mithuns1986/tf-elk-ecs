be0dff577887e131f559fb05580a415dd6274947
Mithun <mithun.s@sgtradex.com>
2022-12-05 16:26:28 +0530

    Jenkinsfile is modified

--- provider.tf
+++ provider.tf
@@ -4,6 +4,6 @@
 # Specify the provider and access details
 provider "aws" {
   #shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "default"
+  #profile                 = "default"
   region                  = var.aws_region
 }
