5f4e8239c0d73b40c2aba79a04a0063eee0a6337
Mithun <mithun.s@sgtradex.com>
2022-12-08 12:32:10 +0530

    modified ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -85,7 +85,7 @@ resource "aws_ecs_task_definition" "elasticsearch" {
     {
     environment = [
         {
-          {"name": "DISCOVERY_TYPE", "value": "single-node", "name": "ELASTIC_PASSWORD":, "value": "SGTelk123!", "name": "ELASTICSEARCH_SKIP_TRANSPORT_TLS", "value": "true", "name": "ES_JAVA_OPTS",	"value": "-Xms512m -Xmx512m"}
+          {"name": "DISCOVERY_TYPE", "value": "single-node", "name": "ELASTIC_PASSWORD":, "value": "SGTelk123!", "name": "ELASTICSEARCH_SKIP_TRANSPORT_TLS", "value": "true", "name": "ES_JAVA_OPTS",	"value": "-Xms512m "\"-Xmx512m"}
         }
       ]
     }
